6dbc7e2b768c2c84fa4068422d6bda3252e16ec1
Dan Collins <dcollins@linuxmail.org>
2019-10-24 22:20:49 -0500

    Init commit

---
+++ js/tweenmax_to.js
@@ -0,0 +1,5 @@
+CSSPlugin.defaultTransformPerspective = 400; 
+
+TweenMax.to(".green", 3, {rotationX:360});
+TweenMax.to(".orange", 3, {rotationY:360});
+TweenMax.to(".grey", 3, {x:100, y:100, scale:2, skewX:45, rotation:180});
